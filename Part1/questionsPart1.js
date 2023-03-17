dcf0badf66890a3f5e1c1e4eff60a5344d08fef1
paul-burnell <81319040+paul-burnell@users.noreply.github.com>
2023-03-17 15:44:23 +0000

    Add files via upload

---
+++ Part1/questionsPart1.js
@@ -0,0 +1,85 @@
+const questions = [
+  {
+    question: 'What should be delivered at the end of the first Sprint? \n \
+    A - Potentially shippable product Increment. \n \
+    B - A formulated plan for the subsequence Sprints. \n \
+    C - At least one piece of working software. \n \
+    D - A completely refined Product Backlog with estimations and task breakdown \n \
+    E - A proof of concept. \n \
+    (choose the best answer)',
+    answers: [
+      { text: 'A and C', correct: true },
+      { text: 'A, C and E', correct: false },
+      { text: 'C and D', correct: false },
+      { text: 'All of the above', correct: false }
+    ]
+  },
+  {
+    question: 'The three pillars of empirical process control consist of: \n \
+    (choose the best answer)?',
+    answers: [
+      { text: 'Planning, Inspection, Adaptation', correct: false },
+      { text: 'Inspection, Transparency, Adaptation', correct: true },
+      { text: 'Planning, Demonstration, Retrospective', correct: false },
+      { text: 'Respect for People, Kaizen, Eliminating Waste', correct: false }
+    ]
+  },
+  {
+    question: 'Which best describes the management style used in the Scrum framework? \n \
+    (choose the best answer)',
+    answers: [
+      { text: 'Authoritarian', correct: false },
+      { text: 'Decentralized', correct: true },
+      { text: 'Persuasive', correct: false },
+      { text: 'Assertive', correct: false }
+    ]
+  },
+  {
+    question: 'Which of the following is NOT an advantage of using Scrum to deliver projects? \n \
+    (choose the best answer)',
+    answers: [
+      { text: 'It makes projects adaptable and open to incorporating change.', correct: false },
+      { text: 'It minimizes non-essential work and leads to higher efficiency levels.', correct: false },
+      { text: 'It leads to greater levels of motivation among employees.', correct: false },
+      { text: 'It ensures that all requirements are delivered at the end of the project.', correct: true }
+    ]
+  },
+  {
+    question: 'Which statement best describes Scrum? \n \
+    (choose the best answer)',
+    answers: [
+      { text: 'A clearly defined methodology that defines the software development process.', correct: false },
+      { text: 'A manual for defining best practices for software development.', correct: false },
+      { text: 'A clearly defined and predictable process that follows the principles of Computer Science.', correct: false },
+      { text: 'A framework to address complex problems in complex environments.', correct: true }
+    ]
+  },
+  {
+    question: 'Scrum is based on which of the following? \n \
+    (choose the best answer)',
+    answers: [
+      { text: 'Defined process.', correct: false },
+      { text: 'Complex process.', correct: false },
+      { text: 'Empiricism.', correct: true },
+      { text: 'Hybrid model.', correct: false }
+    ]
+  },
+  {
+    question: 'Which Scrum Value impacts trust? \n \
+    A - Focus \n \
+    B - Respect \n \
+    C - Openess \n \
+    D - Courage \n \
+    E - Commitment \n \
+    (choose the best answer)',
+    answers: [
+      { text: 'A and B', correct: false },
+      { text: 'A and D', correct: false },
+      { text: 'C, D, and E', correct: false },
+      { text: 'E', correct: false },
+      { text: 'C and E', correct: false },
+      { text: 'B and E', correct: false },
+      { text: 'All of the above', correct: true }
+    ]
+  }
+]
---
+++ Part4/questionsPart4.js
@@ -0,0 +1,67 @@
+const questions = [
+  {
+    question: 'What is sustainable pace? \n \
+    (choose the best answer)?',
+    answers: [
+      { text: 'The pace at which the Scrum Team can work and sustain comfortably for an indefinite period of time.', correct: true },
+      { text: 'The pace at which the Scrum Team converts User Stories into deliverables in a Sprint.', correct: false },
+      { text: 'The pace at which the Scrum Team releases project deliverables.', correct: false },
+      { text: 'The pace at which the Scrum Team can work and sustain comfortably in a set of Sprints leading \
+              to a product release.', correct: false }
+    ]
+  },
+  {
+    question: 'Under what circumstances would a Sprint be cancelled? \n \
+    (choose the best answer)?',
+    answers: [
+      { text: 'When the forecasted workload becomes unachievable.', correct: false },
+      { text: 'When the Product Owner is absent too often.', correct: false },
+      { text: 'When the Developers are unclear on what to do on the remaining work.', correct: false },
+      { text: 'When the Sprint Goal becomes obsolete.', correct: true }
+    ]
+  },
+  {
+    question: 'Who can cancel a sprint? \n \
+    (choose the best answer)?',
+    answers: [
+      { text: 'The Developers', correct: false },
+      { text: 'The Scrum Master', correct: false },
+      { text: 'The Product Owner', correct: true },
+      { text: 'The Project Manager', correct: false }
+    ]
+  },
+  {
+    question: 'Which statement best describes a Product Owner\'s responsibility? \n \
+    (choose the best answer)?',
+    answers: [
+      { text: 'Keep stakeholders from distracting the Developers.', correct: false },
+      { text: 'Optimizing the value of the work the Scrum Team does.', correct: true },
+      { text: 'Managing the project and ensuring that the work meets the commitments to the stakeholders.', correct: false },
+      { text: 'Directing the Developers.', correct: false }
+    ]
+  },
+  {
+    question: 'What is the function or purpose of Management in Scrum? \n \
+    (choose the best answer)?',
+    answers: [
+      { text: 'Identify and remove people that are not working hard enough.', correct: false },
+      { text: 'Monitor the progress of the Developers on the Scrum Team.', correct: false },
+      { text: 'Support the Product Owner with insights and information into high value product and \
+              system capabilities. Support the Scrum Master to encourage organizational change \
+              that fosters empiricism, self-management, bottom-up intelligence, and intelligent \
+              product delivery.', correct: true },
+      { text: 'Continually monitor staffing levels of the Scrum Team.', correct: false }
+    ]
+  },
+  {
+    question: 'When is ownership of individual items, in the Sprint Backlog, assigned to individual Developers? \n \
+    (choose the best answer)?',
+    answers: [
+      { text: 'Any time during Sprint Planning.', correct: false },
+      { text: 'Never. Even if individuals work on specific Sprint Backlog items, the Developers as \
+              a whole remain accountable for the Sprint Backlog in its entirety.', correct: true },
+      { text: 'During the Daily Scrum.', correct: false },
+      { text: 'When the Scrum Master assigns it.', correct: false }
+    ]
+  }
+]
